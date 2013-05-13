4c1887a801a34e84e1928989d8ca4d6d458a1247
Russell Gaspard <russg@fullsail.edu>
2013-05-12 22:49:43 -0400

    set up for week2 apps

---
+++ Week2/BRONZE/js/json.js
@@ -0,0 +1,74 @@
+// Russell Gaspard
+// Project 4
+// Visual Frameworks - VFW 1304
+// Mobile Development
+// Full Sail University
+
+var json = {
+	"musician1":{
+		"fname":	["First Name:", "Russ"],	
+		"lname":	["Last Name:", "Gaspard"],	
+		"phone":	["Phone:", "321-439-0605"],		
+		"email":	["Email:", "russg@fullsail.edu"],			
+		"primary":	["Primary Instrument:", "Guitar"],	
+		"all":		["Additional Instruments:", "Other", "Keyboard"], 			
+		"rating":	["Rating:", "10"],	
+		"lgig":		["Last Gig:", "2013-01-1"],	
+		"notes":	["Notes:", "Russ also plays fiddle, mandolin and harmonica."]	
+	},
+	"musician2":{
+		"fname":	["First Name:", "Dave"],	
+		"lname":	["Last Name:", "Cox"],	
+		"phone":	["Phone:", "321-331-2791"],		
+		"email":	["Email:", "dcox@bassman.com"],			
+		"primary":	["Primary Instrument:", "Bass"],	
+		"all":		["Additional Instruments:", "Keyboard", "Drums"], 			
+		"rating":	["Rating:", "10"],	
+		"lgig":		["Last Gig:", "2013-05-5"],	
+		"notes":	["Notes:", "Dave is not as interested in learning material but always ready to jam."]	
+	},
+	"musician3":{
+		"fname":	["First Name:", "Dave"],	
+		"lname":	["Last Name:", "Gibbs"],	
+		"phone":	["Phone:", "407-529-8561"],		
+		"email":	["Email:", "dgibbs@fullsail.com"],			
+		"primary":	["Primary Instrument:", "Keys"],	
+		"all":		["Additional Instruments:", "Guitar"], 			
+		"rating":	["Rating:", "10"],	
+		"lgig":		["Last Gig:", "2012-06-16"],		
+		"notes":	["Notes:", "Hard to get Dave to commit to a gig but always worth it."]	
+	},
+	"musician4":{
+		"fname":	["First Name:", "John Paul"],	
+		"lname":	["Last Name:", "McCune"],	
+		"phone":	["Phone:", "321-806-8618"],		
+		"email":	["Email:", "jp@fit.com"],			
+		"primary":	["Primary Instrument:", "Drums"],	
+		"all":		["Additional Instruments:", "Guitar"], 			
+		"rating":	["Rating:", "10"],	
+		"lgig":		["Last Gig:", "2013-04-15"],	
+		"notes":	["Notes:", "Love this guy."]	
+	},
+	"musician5":{
+		"fname":	["First Name:", "Jenn"],	
+		"lname":	["Last Name:", "Willsey"],	
+		"phone":	["Phone:", "407-252-4264"],		
+		"email":	["Email:", "jwillsey@redhead.gov"],			
+		"primary":	["Primary Instrument:", "Vocals"],	
+		"all":		["Additional Instruments:", "Drums"], 			
+		"rating":	["Rating:", "10"],	
+		"lgig":		["Last Gig:", "2013-02-7"],		
+		"notes":	["Notes:", "Sings a fine lead but incredible harmony as well."]	
+	},
+	"musician6":{
+		"fname":	["First Name:", "Matt"],	
+		"lname":	["Last Name:", "Tuten"],	
+		"phone":	["Phone:", "407-310-7424"],		
+		"email":	["Email:", "mtuten@gmail.com"],			
+		"primary":	["Primary Instrument:", "Other"],	
+		"all":		["Additional Instruments:", "Guitar"], 			
+		"rating":	["Rating:", "10"],	
+		"lgig":		["Last Gig:", "2011-12-31"],	
+		"notes":	["Notes:", "Mandolin player."]	
+	}
+}
